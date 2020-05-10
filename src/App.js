815b76acc5ad0ab4a320bd296358ac467a466925
Davidson Poole <davidson@Davidsons-MacBook-Pro.local>
2020-05-09 23:18:28 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import pineapple from './pineapple.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pineapple} className="App-logo" alt="logo" />
+        <p>
+          Cultured.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
